8f250103338896f0dce9dac94cd8f691b2376de6
Malex <malexprojects@gmail.com>
2010-03-29 16:41:57 +0200

    crawler first design [malex]

---
+++ include/crawler.hpp
@@ -0,0 +1,32 @@
+#ifndef LYRICS_CRAWLER_HPP
+#define LYRICS_CRAWLER_HPP
+
+#include <curl/curl.h>
+
+enum sitemode {
+	ChartLyrics = 0;
+};
+
+class crawler {
+
+	public:
+
+	lyric getLyric(sitemode site, string auth, string title);
+
+	private:
+
+	CURL* curl;
+	CURLcode res;
+
+	/* e qua mi stavo impiantando
+	string getText(
+	*/
+
+	//instanzia curl
+	void initCURL() {
+		this->curl = curl_easy_init();
+	}
+
+
+};
+#endif // LYRICS_CRAWLER_HPP
