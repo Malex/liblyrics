71292293bac1e5d3ade444b10400ca4360daa3c5
Malex <malexprojects@gmail.com>
2010-04-02 23:24:26 +0200

    Ops dimenticanza

--- src/lyrics.cpp
+++ src/lyrics.cpp
@@ -17,7 +17,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-//commento inutile
+
 #include <string>
 #include "lyrics.hpp"
 
